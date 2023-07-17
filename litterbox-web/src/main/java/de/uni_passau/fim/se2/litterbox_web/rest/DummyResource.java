a851d0d005d028b1877b51816fdf945742160ae4
Benedikt Fein <fein@fim.uni-passau.de>
2023-07-17 17:07:41 +0200

    chore: set up autoformatter
    Closes #1

--- litterbox-web/src/main/java/de/uni_passau/fim/se2/litterbox_web/rest/DummyResource.java
+++ litterbox-web/src/main/java/de/uni_passau/fim/se2/litterbox_web/rest/DummyResource.java
@@ -1,18 +1,38 @@
+/*
+ * Copyright (C) 2023 LitterBox contributors
+ *
+ * This file is part of LitterBox.
+ *
+ * LitterBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * LitterBox is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LitterBox. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.uni_passau.fim.se2.litterbox_web.rest;
 
-import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
-import de.uni_passau.fim.se2.litterbox.ast.model.Program;
-import de.uni_passau.fim.se2.litterbox.ast.parser.Scratch3Parser;
+import java.io.File;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.IOException;
+import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
+import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.parser.Scratch3Parser;
 
 @RestController
 public class DummyResource {
+
     private static final Logger log = LoggerFactory.getLogger(DummyResource.class);
 
     private static final Scratch3Parser PARSER = new Scratch3Parser();
