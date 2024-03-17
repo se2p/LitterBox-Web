90a78d46d5c57b001dbd72f10682bc7a24b8ba0c
Benedikt Fein <fein@fim.uni-passau.de>
2024-03-17 21:32:10 +0100

    test: add additional ArchUnit tests
    - Logger fields should have correct name and modifier
    - Use services for Scratch parsing and temporary files

---
+++ src/test/java/de/uni_passau/fim/se2/litterbox_web/architecture/GeneralRulesTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2024 LitterBox-Web contributors
+ *
+ * This file is part of LitterBox-Web.
+ *
+ * SPDX-FileCopyrightText: 2024 LitterBox-Web contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * LitterBox-Web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public Licence as published by
+ * the Free Software Foundation, either version 3 of the Licence, or (at
+ * your option) any later version.
+ *
+ * LitterBox-Web is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence
+ * along with LitterBox-Web. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.uni_passau.fim.se2.litterbox_web.architecture;
+
+import static com.tngtech.archunit.base.DescribedPredicate.not;
+import static com.tngtech.archunit.core.domain.JavaCall.Predicates.target;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.assignableTo;
+import static com.tngtech.archunit.core.domain.properties.HasOwner.Predicates.With.owner;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.GeneralCodingRules;
+
+import de.uni_passau.fim.se2.litterbox.ast.parser.Scratch3Parser;
+import de.uni_passau.fim.se2.litterbox_web.model.ScratchProgramConverter;
+import de.uni_passau.fim.se2.litterbox_web.shared.Scratch3ParserService;
+import de.uni_passau.fim.se2.litterbox_web.shared.TemporaryFileService;
+
+class GeneralRulesTest extends ArchitectureTest {
+
+    @Test
+    void doNotUseStandardStreams() {
+        GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS.check(allClasses);
+    }
+
+    @Test
+    void noCollectorsToList() {
+        final ArchRule toListUsage = noClasses()
+            .should()
+            .callMethod(Collectors.class, "toList")
+            .because("you should use .toList() or .collect(Collectors.toCollection(ArrayList::new)) instead");
+
+        toListUsage.check(allClasses);
+    }
+
+    @Test
+    void noDirectScratchParserUsage() {
+        final ArchRule parseUsage = noClasses()
+            .should().callMethodWhere(target(owner(assignableTo(Scratch3Parser.class))))
+            .because("the Scratch3ParserService or the ScratchProgramConverter should be used instead");
+
+        final JavaClasses target = productionClasses.that(
+            not(
+                assignableTo(Scratch3ParserService.class)
+                    .or(assignableTo(ScratchProgramConverter.SerializeConverter.class))
+                    .or(assignableTo(ScratchProgramConverter.DeserializeConverter.class))
+            )
+        );
+        parseUsage.check(target);
+    }
+
+    @Test
+    void noTempFileCreation() {
+        final ArchRule rule = noClasses()
+            .should().callMethod(Files.class, "createTempFile")
+            .orShould().callMethod(Files.class, "createTempDirectory")
+            .because("the TemporaryFileService should be used");
+
+        final JavaClasses target = productionClasses.that(not(assignableTo(TemporaryFileService.class)));
+        rule.check(target);
+    }
+}
--- src/test/java/de/uni_passau/fim/se2/litterbox_web/architecture/LoggerTest.java
+++ src/test/java/de/uni_passau/fim/se2/litterbox_web/architecture/LoggerTest.java
@@ -21,19 +21,39 @@
  */
 package de.uni_passau.fim.se2.litterbox_web.architecture;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.base.DescribedPredicate.not;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.INTERFACES;
+import static com.tngtech.archunit.lang.conditions.ArchPredicates.are;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.GeneralCodingRules;
 
-class ImportsTest extends ArchitectureTest {
+class LoggerTest extends ArchitectureTest {
 
     @Test
     void forbidJavaLoggerImport() {
-        final ArchRule rule = noClasses().should()
-            .dependOnClassesThat().haveNameMatching("java.util.logging.Logger")
+        final ArchRule rule = GeneralCodingRules.NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING
             .because("the slf4j Logger and LoggerFactory should be used instead");
         rule.check(allClasses);
+    }
+
+    @Test
+    void testCorrectLoggerFields() {
+        final ArchRule namingRule = fields().that().haveRawType(Logger.class).should().haveName("log");
+        final ArchRule modifiersRule = fields().that().haveRawType(Logger.class)
+            .should().bePrivate()
+            .andShould().beFinal()
+            .andShould().beStatic();
+
+        namingRule.check(allClasses);
+
+        // Interfaces can only contain public attributes
+        final JavaClasses modifierExclusions = allClasses.that(are(not(INTERFACES)));
+        modifiersRule.check(modifierExclusions);
     }
 }
