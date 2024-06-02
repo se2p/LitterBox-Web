7822f1d771599f7540aaa9be2a5e0463d5f38594
Benedikt Fein <fein@fim.uni-passau.de>
2024-06-02 17:13:30 +0200

    chore: use alejandra to format nix files

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2024 LitterBox-Web contributors
 #
 # SPDX-License-Identifier: EUPL-1.2
-
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,112 +14,108 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      javaVersion = 21;
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            jdk = pkgs."jdk${toString javaVersion}_headless";
-            maven = (pkgs.maven.override { jdk = jdk; });
-            litterbox-web-jar = maven.buildMavenPackage rec {
-              pname = "litterbox-web";
-              version = "0.0.1-SNAPSHOT";
-              nativeBuildInputs = [
-                jdk
-                maven
-                pkgs.stripJavaArchivesHook
-              ];
-              src = ./.;
-              buildOffline = true;
-              mvnHash = "sha256-TqeB+PX8qysMzpRZJexdAe4wq+6X2ifQnQHo3S0hVYo=";
-              mvnParameters = "-DskipTests";
-              installPhase = ''
-                mkdir -p $out
-                cp target/litterbox-web-${version}.jar $out/
-              '';
-            };
-            jv = builtins.trace ''version: ${pkgs.lib.versions.major jdk.version}'' javaVersion;
-          in
-          {
-            devenv-up = self.devShells.${system}.default.config.procfileScript;
-            default = litterbox-web-jar;
-            litterbox-web-container = import ./scripts/nix/container.nix {
-              inherit pkgs jdk litterbox-web-jar;
-            };
-          });
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    javaVersion = 21;
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        jdk = pkgs."jdk${toString javaVersion}_headless";
+        maven = pkgs.maven.override {jdk = jdk;};
+        litterbox-web-jar = maven.buildMavenPackage rec {
+          pname = "litterbox-web";
+          version = "0.0.1-SNAPSHOT";
+          nativeBuildInputs = [
+            jdk
+            maven
+            pkgs.stripJavaArchivesHook
+          ];
+          src = ./.;
+          buildOffline = true;
+          mvnHash = "sha256-TqeB+PX8qysMzpRZJexdAe4wq+6X2ifQnQHo3S0hVYo=";
+          mvnParameters = "-DskipTests";
+          installPhase = ''
+            mkdir -p $out
+            cp target/litterbox-web-${version}.jar $out/
+          '';
+        };
+      in {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        default = litterbox-web-jar;
+        litterbox-web-container = import ./scripts/nix/container.nix {
+          inherit pkgs jdk litterbox-web-jar;
+        };
+      });
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            jdk = pkgs."jdk${toString javaVersion}_headless";
-            mvn-lint = pkgs.writeScriptBin "mvn-lint" ''
-              set -euo pipefail
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        jdk = pkgs."jdk${toString javaVersion}_headless";
+        mvn-lint = pkgs.writeScriptBin "mvn-lint" ''
+          set -euo pipefail
 
-              mvn license:format spotless:apply checkstyle:check
-            '';
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  packages = [
-                    pkgs.reuse
-                    mvn-lint
-                  ];
-                  languages.java = {
-                    enable = true;
-                    jdk.package = jdk;
-                    maven.enable = true;
-                  };
-                  pre-commit.hooks = {
-                    reuse-lint = {
-                      enable = true;
-                      name = "reuse-lint";
-                      description = "Run reuse lint licence header checker.";
-                      entry = "${pkgs.reuse}/bin/reuse lint";
-                      pass_filenames = false;
-                    };
-                    mvn-licence = {
-                      enable = true;
-                      name = "mvn-licence";
-                      description = "Run maven license header checker.";
-                      entry = "${pkgs.maven}/bin/mvn license:check";
-                      files = "^src/.*";
-                      pass_filenames = false;
-                    };
-                    spotless-fmt = {
-                      enable = true;
-                      name = "spotless-fmt";
-                      description = "Check for correct Java source code formatting.";
-                      entry = "${pkgs.maven}/bin/mvn spotless:check";
-                      files = ".*\\.java$";
-                      pass_filenames = false;
-                    };
-                    mvn-checkstyle = {
-                      enable = true;
-                      name = "mvn-checkstyle";
-                      description = "Run Checkstyle checker.";
-                      entry = "${pkgs.maven}/bin/mvn checkstyle:check";
-                      files = ".*\\.java$";
-                      pass_filenames = false;
-                    };
-                    nixpkgs-fmt = {
-                      enable = true;
-                      name = "nixpkgs-fmt";
-                      files = ".*\\.nix$";
-                      entry = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check";
-                    };
-                  };
-                }
+          mvn license:format spotless:apply checkstyle:check
+        '';
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = [
+                pkgs.reuse
+                mvn-lint
               ];
-            };
-          });
-    };
+              languages.java = {
+                enable = true;
+                jdk.package = jdk;
+                maven.enable = true;
+              };
+              pre-commit.hooks = {
+                alejandra.enable = true;
+                reuse-lint = {
+                  enable = true;
+                  name = "reuse-lint";
+                  description = "Run reuse lint licence header checker.";
+                  entry = "${pkgs.reuse}/bin/reuse lint";
+                  pass_filenames = false;
+                };
+                mvn-licence = {
+                  enable = true;
+                  name = "mvn-licence";
+                  description = "Run maven license header checker.";
+                  entry = "${pkgs.maven}/bin/mvn license:check";
+                  files = "^src/.*";
+                  pass_filenames = false;
+                };
+                spotless-fmt = {
+                  enable = true;
+                  name = "spotless-fmt";
+                  description = "Check for correct Java source code formatting.";
+                  entry = "${pkgs.maven}/bin/mvn spotless:check";
+                  files = ".*\\.java$";
+                  pass_filenames = false;
+                };
+                mvn-checkstyle = {
+                  enable = true;
+                  name = "mvn-checkstyle";
+                  description = "Run Checkstyle checker.";
+                  entry = "${pkgs.maven}/bin/mvn checkstyle:check";
+                  files = ".*\\.java$";
+                  pass_filenames = false;
+                };
+              };
+            }
+          ];
+        };
+      });
+  };
 }
--- scripts/nix/container.nix
+++ scripts/nix/container.nix
@@ -1,13 +1,16 @@
 # SPDX-FileCopyrightText: 2024 LitterBox-Web contributors
 #
 # SPDX-License-Identifier: EUPL-1.2
-
-{ pkgs, jdk, litterbox-web-jar, ... }:
-let
+{
+  pkgs,
+  jdk,
+  litterbox-web-jar,
+  ...
+}: let
   javaVersion = pkgs.lib.versions.major jdk.version;
   litterbox-web-jar-module-deps = pkgs.stdenv.mkDerivation {
     name = "litterbox-web-jar-module-deps";
-    nativeBuildInputs = [ jdk litterbox-web-jar ];
+    nativeBuildInputs = [jdk litterbox-web-jar];
     src = ./.;
     buildPhase = ''
       jar xf ${litterbox-web-jar}/litterbox-web-*.jar
@@ -34,21 +37,21 @@ let
     );
   };
 in
-pkgs.dockerTools.buildImage {
-  name = "litterbox-web";
-  tag = "${litterbox-web-jar.version}";
-  extraCommands = ''
-    mkdir -p -m 777 tmp
-  '';
-  config = {
-    ExposedPorts = {
-      "8080/tcp" = { };
+  pkgs.dockerTools.buildLayeredImage {
+    name = "litterbox-web";
+    tag = "${litterbox-web-jar.version}";
+    extraCommands = ''
+      mkdir -p -m 777 tmp
+    '';
+    config = {
+      ExposedPorts = {
+        "8080/tcp" = {};
+      };
+      WorkingDir = "/app";
+      Entrypoint = [
+        "${jre}/bin/java"
+        "-jar"
+        "${litterbox-web-jar}/litterbox-web-${litterbox-web-jar.version}.jar"
+      ];
     };
-    WorkingDir = "/app";
-    Entrypoint = [
-      "${jre}/bin/java"
-      "-jar"
-      "${litterbox-web-jar}/litterbox-web-${litterbox-web-jar.version}.jar"
-    ];
-  };
-}
+  }
