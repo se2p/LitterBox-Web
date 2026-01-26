27f40374e046129772a40adf352f2ac2a781334c
Benedikt Fein <fein@fim.uni-passau.de>
2026-01-26 09:05:18 +0100

    chore: update licence year in Java files

--- litterbox-web/src/main/java/de/uni_passau/fim/se2/litterbox_web/shared/connectors/HttpClientBadRequestException.java
+++ litterbox-web/src/main/java/de/uni_passau/fim/se2/litterbox_web/shared/connectors/HttpClientBadRequestException.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2024 LitterBox-Web contributors
+ * Copyright (C) 2024-2026 LitterBox-Web contributors
  *
  * This file is part of LitterBox-Web.
  * Licenced under the EUPL-1.2 or later.
  *
- * SPDX-FileCopyrightText: 2024 LitterBox-Web contributors
+ * SPDX-FileCopyrightText: 2024-2026 LitterBox-Web contributors
  * SPDX-License-Identifier: EUPL-1.2
  */
 package de.uni_passau.fim.se2.litterbox_web.shared.connectors;
